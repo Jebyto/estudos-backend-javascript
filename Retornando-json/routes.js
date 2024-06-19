425fe39f305ba8438d8dcf9d3d5ac8ed05848fb5
josé carlos <jose-carlos64@live.com>
2024-06-18 23:26:21 -0300

    metodo get

--- Retornando-json/routes.js
+++ Retornando-json/routes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const gamesController = require('./controllers/games-controller')
+const gamesController = require('./controllers/gameController')
 
 const router = express.Router()
 
